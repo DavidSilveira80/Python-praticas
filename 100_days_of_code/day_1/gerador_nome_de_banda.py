ed600bc42b98a95fb915e7eaf6c66f46e347b81e
DavidSilveira80 <davidpscontato@gmail.com>
2025-03-19 11:07:48 -0300

    Remoções

--- 100_days_of_code/day_1/gerador_nome_de_banda.py
+++
@@ -1,4 +0,0 @@
-print('Bem-vindo ao seu Gerador de Nome de Banda.')
-cidade = input('Qual é o nome da sua cidade? ')
-nome_do_seu_pet = input('Qual é o nome do seu Pet? ')
-print(f'O nome da sua Banda será: {cidade} {nome_do_seu_pet}')
